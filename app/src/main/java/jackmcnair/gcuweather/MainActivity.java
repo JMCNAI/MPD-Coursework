4c74ea5910933bfe8b89aa00a910c94af3b1cfdd
JMCNAI <jackmcnair99@gmail.com>
2020-08-17 20:59:22 +0200

    added detailed view

--- app/src/main/java/jackmcnair/gcuweather/MainActivity.java
+++ app/src/main/java/jackmcnair/gcuweather/MainActivity.java
@@ -1,12 +1,14 @@
 package jackmcnair.gcuweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,24 +26,75 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private ListView lvRss;
     private ArrayList<WeatherItem> items = new ArrayList<>();
     private String baseUrl = "https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/";
     private String[] locationIDs = new String[]{"2648579", "2643743", "5128581", "287286", "934154", "1185241"};
     private String[] locationNames = new String[]{"Glasgow", "London", "New York", "Oman", "Mauritius", "Bangladesh"};
+    private Button backButton;
+    private ConstraintLayout detailLayout;
+    private TextView locationName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvRss = findViewById(R.id.lvRss);
+        backButton = findViewById(R.id.back);
+        detailLayout = findViewById(R.id.layout);
+        locationName = findViewById(R.id.locationName);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lvRss.setVisibility(View.VISIBLE);
+                detailLayout.setVisibility(View.GONE);
+            }
+        });
 
         lvRss.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                lvRss.setVisibility(View.GONE);
+                detailLayout.setVisibility(View.VISIBLE);
 
+                locationName.setText(items.get(i).getLocationName());
+
+                TextView day1 = findViewById(R.id.day1);
+                day1.setText(items.get(i).getDay().get(0));
+                TextView day2 = findViewById(R.id.day2);
+                day2.setText(items.get(i).getDay().get(1));
+                TextView day3 = findViewById(R.id.day3);
+                day3.setText(items.get(i).getDay().get(2));
+
+                TextView temp1 = findViewById(R.id.temp1);
+                temp1.setText(items.get(i).getMintemp().get(0));
+                TextView temp2 = findViewById(R.id.temp2);
+                temp2.setText(items.get(i).getMintemp().get(1));
+                TextView temp3 = findViewById(R.id.temp3);
+                temp3.setText(items.get(i).getMintemp().get(2));
+
+
+                TextView maxTemp2 = findViewById(R.id.maxTemp2);
+                maxTemp2.setText(items.get(i).getMaxtemp().get(0));
+                TextView maxTemp3 = findViewById(R.id.maxTemp3);
+                maxTemp3.setText(items.get(i).getMaxtemp().get(1));
+
+                TextView rain1 = findViewById(R.id.rain1);
+                rain1.setText(items.get(i).getRain().get(0));
+                TextView rain2 = findViewById(R.id.rain2);
+                rain2.setText(items.get(i).getRain().get(1));
+                TextView rain3 = findViewById(R.id.rain3);
+                rain3.setText(items.get(i).getRain().get(2));
+
+                TextView windspeed1 = findViewById(R.id.windspeed1);
+                windspeed1.setText(items.get(i).getWindspeed().get(0));
+                TextView windspeed2 = findViewById(R.id.windspeed2);
+                windspeed2.setText(items.get(i).getWindspeed().get(1));
+                TextView windspeed3 = findViewById(R.id.windspeed3);
+                windspeed3.setText(items.get(i).getWindspeed().get(2));
             }
         });
 
@@ -100,7 +153,6 @@ public class MainActivity extends AppCompatActivity {
                                 if (insideItem) {
                                     String description = xpp.nextText();
                                     String[] descriptionParts = description.split(", ");
-                                    weatherItem.getRain().add(descriptionParts[0].split(": ")[1]);
 
                                     String[] partTitles = {"Maximum Temperature",
                                             "Minimum Temperature", "Wind Direction",
