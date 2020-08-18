ecad46c242b3d1858ad3819ef8b268f7c2eca4f7
JMCNAI <jackmcnair99@gmail.com>
2020-08-18 19:58:32 +0200

    scroll fix in landscape

--- app/src/main/java/jackmcnair/gcuweather/MainActivity.java
+++ app/src/main/java/jackmcnair/gcuweather/MainActivity.java
@@ -7,7 +7,6 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
